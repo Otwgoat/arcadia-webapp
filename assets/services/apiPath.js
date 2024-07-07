80420f692686ed9677fda121ec4ab9047a5e6f5d
Otwgoat <lucas.jouffroy@gmail.com>
2024-07-07 14:57:10 +0200

    chore: Use environment variable for API path in apiPath.js

--- assets/services/apiPath.js
+++ assets/services/apiPath.js
@@ -1,5 +1,10 @@
 function apiPath (path) {
-    return 'http://localhost:8000/api/' + path;
+    if (process.env.NODE_ENV === 'development') {
+        return 'http://localhost:8000/api/' + path;
+    } else {
+        return 'https://arcadia-webapp-80ad1704e101.herokuapp.com/api/' + path;
+    }
+    
 }
 
 export default apiPath;
